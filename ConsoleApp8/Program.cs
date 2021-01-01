a53dd2c79cdd34d4539412c8ccfbe2ca31167cd5
Resul Taşköprü <resultaskpru@gmail.com>
2021-01-01 14:41:37 +0300

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vize = Convert.ToDouble(Console.ReadLine());
+            double final = Convert.ToDouble(Console.ReadLine());
+            double ortalama = vize * 0.4 + final * 0.6;
+            if (ortalama >= 50 && final >= 50)
+                Console.WriteLine("Geçtiniz,ortalama puan = " + ortalama);
+            else if (ortalama < 50)
+                Console.WriteLine("Kaldınız" + ortalama);
+            else if (ortalama >= 50 && final < 50)
+                Console.WriteLine("final notunuz 50'den küçük,Kaldınız" + ortalama);
+
+        }
+    }
+}
